b9191d487c5f2ce28e1130d711857af29f034f60
RegisEmiel <regisemiel@yandex.ru>
2022-05-24 18:36:28 +0300

    Init commit

---
+++ src/main/java/StrategyClient.java
@@ -0,0 +1,12 @@
+/**
+ * Реализация поведенческого паттерна Стратегия на примере компораторов в алгоритме сортировки массива
+ */
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class StrategyClient {
+    public static void main(String[] args) {
+
+    }
+}
